e8a16836d7c70eb60d93a92dba189d50ee17d542
evgeniy-bokshits <evgeniy.bokshits@gmail.com>
2022-07-30 17:56:06 +0300

    init commit

---
+++ Task_2/Catalog_Service/BLL/Implementations/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using Core.Infrastructure.Models;
+using BLL.Interfaces;
+using DAL.Interfaces;
+
+namespace BLL.Implementations;
+
+public class ProductService : IProductService
+{
+    const int Skip = 0;
+    const int Count = 10;
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository) => _productRepository = productRepository;
+
+    public async Task<Product> AddProduct(Product product)
+    {
+        return await _productRepository.AddProduct(product);
+    }
+
+    public async Task DeleteProduct(int productId)
+    {
+        await _productRepository.DeleteProduct(productId);
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts(int categoryId, int skip = Skip, int count = Count)
+    {
+        return await _productRepository.GetProducts(categoryId, skip, count);
+    }
+
+    public async Task<Product> UpdateProduct(Product product)
+    {
+        return await _productRepository.UpdateProduct(product);
+    }
+}
